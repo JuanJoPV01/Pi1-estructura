27cc2516e589cbd6ac1127e42cefbe0811d6eb46
Juan Puello <jpuellov@unicartagena.edu.co>
2026-09-16 14:09:02 -0500

    feat(bloque1): implementacion completa de ejercicios 1 al 5

--- src/main/java/com/ejercicio24/Main.java
+++ src/main/java/com/ejercicio24/Main.java
@@ -1,109 +1,51 @@
 package com.ejercicio24;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("====== 1. PARTE 1: ARREGLOS (UNIDIMENSIONALES) ======");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
 
+        do {
+            System.out.println("\n=================================");
+            System.out.println("   MENÚ DE ACTIVIDADES (1 AL 5)  ");
+            System.out.println("=================================");
+            System.out.println("1. Analizador de Calificaciones");
+            System.out.println("2. Compresor Simple de Texto");
+            System.out.println("3. Juego del Ahorcado");
+            System.out.println("4. Sistema de Inventario");
+            System.out.println("5. Agenda Telefónica");
+            System.out.println("0. Salir");
+            System.out.print("Selecciona el ejercicio a ejecutar (0-5): ");
 
-        int[] arreglos = new int[10];
-        Random rand = new Random();
-        for (int i = 0; i < arreglos.length; i++) {
-            arreglos[i] = rand.nextInt(50) + 1;
-        }
+            opcion = sc.nextInt();
 
-
-        System.out.println("\n--- Recorrido For Clásico ---");
-        for (int i = 0; i < arreglos.length; i++) {
-            System.out.println("Índice [" + i + "]: " + arreglos[i]);
-        }
-
-
-        System.out.println("\n--- Recorrido For-Each ---");
-        for (int val : arreglos) {
-            System.out.print(val + " ");
-        }
-        System.out.println("\n");
-
-
-        int[] imparesACero = arreglos.clone();
-        for (int i = 0; i < imparesACero.length; i++) {
-            if (imparesACero[i] % 2 != 0) {
-                imparesACero[i] = 0;
+            switch (opcion) {
+                case 1:
+                    EjerciciosGrupo1.ejercicio1(sc);
+                    break;
+                case 2:
+                    EjerciciosGrupo1.ejercicio2(sc);
+                    break;
+                case 3:
+                    EjerciciosGrupo1.ejercicio3(sc);
+                    break;
+                case 4:
+                    EjerciciosGrupo1.ejercicio4();
+                    break;
+                case 5:
+                    EjerciciosGrupo1.ejercicio5(sc);
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa.");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
             }
-        }
-        System.out.println("Original:          " + Arrays.toString(arreglos));
-        System.out.println("Impares por cero:  " + Arrays.toString(imparesACero));
+        } while (opcion != 0);
 
-
-        int[] multIndice = arreglos.clone();
-        for (int i = 0; i < multIndice.length; i++) {
-            multIndice[i] = multIndice[i] * i;
-        }
-        System.out.println("Multiplicado x pos: " + Arrays.toString(multIndice));
-
-
-        int objetivo = arreglos[2];
-        int posEncontrada = busquedaLineal(arreglos, objetivo);
-        System.out.println("\nBúsqueda lineal del valor " + objetivo + ": Encontrado en índice " + posEncontrada);
-
-
-        System.out.println("\n====== 2. PARTE 2: MATRICES (BIDIMENSIONALES) ======");
-
-
-        int[][] matriz = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-
-
-        System.out.println("\n--- Matriz en formato Tabla ---");
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + "\t");
-            }
-            System.out.println();
-        }
-
-
-        System.out.print("\n--- Recorrido por Columnas ---\nValores: ");
-        for (int col = 0; col < matriz[0].length; col++) {
-            for (int fila = 0; fila < matriz.length; fila++) {
-                System.out.print(matriz[fila][col] + " ");
-            }
-        }
-        System.out.println("\n");
-
-
-        int sumaTotal = 0;
-        for (int[] fila : matriz) {
-            for (int elemento : fila) {
-                sumaTotal += elemento;
-            }
-        }
-        System.out.println("Suma total de la matriz: " + sumaTotal);
-
-
-        int[] auxFila = matriz[0];
-        matriz[0] = matriz[matriz.length - 1];
-        matriz[matriz.length - 1] = auxFila;
-
-        System.out.println("\n--- Matriz tras intercambiar Fila 1 y Fila 3 ---");
-        for (int[] fila : matriz) {
-            System.out.println(Arrays.toString(fila));
-        }
-    }
-
-
-    public static int busquedaLineal(int[] arr, int objetivo) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == objetivo) {
-                return i;
-            }
-        }
-        return -1;
+        sc.close();
     }
 }
